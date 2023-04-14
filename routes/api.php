622f7989c107f4bc78ca5265fe2f8166567b69d2
Rafael Velasquez <rafa_el171@yahoo.com>
2023-04-14 16:20:02 -0400

    feat: login con JWT

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::group([    
+    'middleware' => ['api'],
+    'prefix' => 'auth'], 
+    function(){
+
+    Route::post('register', [UserController::class, 'register']);
+    Route::post('login', [UserController::class, 'authenticate']);
+});
+
+
+Route::group([
+    
+    'middleware' => ['api','jwt.verify'],
+    'prefix' => 'auth'
+    
+], function ($router) {
+    Route::post('logout', [UserController::class, 'logout']);
+    // Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('user', [UserController::class, 'getAuthenticatedUser']);
 });
