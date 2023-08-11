ecd068570ba79774ab133c54711db849c4049a2e
Rafael Velasquez <rafa_el171@yahoo.com>
2023-08-11 15:49:47 -0400

    feat: CRUD providers

---
+++ app/Models/Provider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Provider extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'rif',
+        'contact',
+        'email',
+        'phone'
+    ];
+
+    public function scopeBusqueda($query, $search)
+    {
+        $query->when($search ?? false, function($query, $s){
+            $query->where('name', 'like', "%$s%")
+                ->orWhere('rif', 'like', "%$s%")
+                ->orWhere('contact', 'like', "%$s%")
+                ->orWhere('email', 'like', "%$s%");
+        });
+
+    }
+}
